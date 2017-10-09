0757d090b33926d3fb7ab7f6eccd6c776eba8d18
Daniel Larsson <dl77sea@Daniels-MacBook-Air.local>
2017-10-09 13:54:39 -0700

    rough pagination on down working

--- public/navbar/navbar.component.js
+++ public/navbar/navbar.component.js
@@ -3,15 +3,79 @@
     .component('navbar', {
       controller: controller,
       template: `
+      <header>
+        <div class="navbar-fixed">
+          <nav>
+            <div class="nav-wrapper">
+              <a ui-sref="posts" class="brand-logo">Logo</a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li>
+                  <a class="modal-trigger" href="#modal-signin">Sign in</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+
+      <main>
+
+        <div class="container">
+          <ui-view></ui-view>
+        </div>
+
+      </main>
+
+      <!-- <footer class="page-footer"> -->
+      <footer class="page-footer">
+        <div class="footer-fixed">
+          <div class="footer-copyright">
+            <div class="container">
+              © 2014 Copyright Text
+              <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </div>
+      </footer>
 
 
 
+ <!-- Sign in Modal -->
+ <div id="modal-signin" class="modal">
+   <div class="modal-content">
+     <h4>Sign In</h4>
+     <div class="container">
+  <div class="row">
+
+    <form id="signin-form" class="col s12">
+
+      <div class="row">
+        <div class="col s12"></div>
+        <div class="input-field">
+          <input id="email" type="text" class="validate">
+          <label for="email">email</label>
+        </div>
+        <div class="col s12"></div>
+        <div class="input-field">
+          <input id="password" type="password" class="validate">
+          <label for="password">password</label>
+        </div>
+      </div>
 
 
-        `
+      <div class="center-align">
+        <button class="btn waves-effect waves-light blue" type="submit" name="action">sign in</button>
+        <p id="info-text" class="center-align">Please sign in or <a href="signup.html">sign up</a></p>
+
+      </div>
+    </form>
+  </div>
+</div>
+   </div>
+ </div>
+      `
     })
   // controller.$inject = ['$state', '$http', '$stateParams'];
-
   // function controller($state, $http, $stateParams) {
   function controller() {
     const vm = this
@@ -19,6 +83,21 @@
 
     vm.$onInit = function() {
       console.log("init navbar")
+
+      $(document).ready(function() {
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('.modal').modal();
+      });
+
+      $("#signin-form").submit(function (e) {
+
+        let email = $("#email").val().trim();
+        let password = $("#password").val().trim();
+        console.log(email, password)
+      });
+
+
+
     };
   }
 
--- public/post/post.component.js
+++ public/post/post.component.js
@@ -5,18 +5,16 @@
       controller: controller,
       template: `
       <a ng-click="$ctrl.test()">posts</a>
-      <div class="row">
 
-        <div class="post-div col s2 m4 l4" ng-repeat="box in $ctrl.boxes">
+      <div id="boxes" class="row">
+        <div class="post-div col s2 m4 l4" ng-repeat="box in $ctrl.curBoxes">
           <p>{{ box.color }}</p>
           <canvas id={{box.id}} class="canvas-style" canvas-init></canvas>
         </div>
-
-        <a ui-sref="edit">create</a>
-        <p>sdf<br><p>sdf<br><p>sdf<br><p>sdf<br><p>sdf<br><p>sdf<br><p>sdf<br>
-        <p>sdf<br><p>sdf<br><p>sdf<br><p>sdf<br><p>sdf<br><p>sdf<br><p>sdf<br>
-
       </div>
+      <a ui-sref="edit">create</a>
+
+      
       `
     })
   // controller.$inject = ['$state', '$http', '$stateParams'];
@@ -50,7 +48,8 @@
     console.log("navbar controller")
 
     vm.$onInit = function() {
-      vm.boxes = [
+
+      vm.allBoxes = [
         {color: "box01"},
         {color: "box02"},
         {color: "box03"},
@@ -62,34 +61,149 @@
         {color: "box09"},
         {color: "box10"},
         {color: "box11"},
-        {color: "box12"}
-        // {color: "box13"},
-        // {color: "box14"},
-        // {color: "box15"},
-        // {color: "box16"}
-        // {color: "box17"},
-        // {color: "box18"},
-        // {color: "box19"}
-        // {color: "box20"},
-        // {color: "box21"},
-        // {color: "box22"}
+        {color: "box12"},
+        {color: "box13"},
+        {color: "box14"},
+        {color: "box15"},
+        {color: "box16"},
+        {color: "box17"},
+        {color: "box18"},
+        {color: "box19"},
+        {color: "box20"},
+        {color: "box21"},
+        {color: "box22"},
+        {color: "box23"},
+        {color: "box24"},
+        {color: "box25"},
+        {color: "box26"},
+        {color: "box27"},
+        {color: "box28"},
+        {color: "box29"},
+        {color: "box30"},
+        {color: "box31"},
+        {color: "box32"},
+        {color: "box33"},
+        {color: "box34"},
+        {color: "box35"},
+        {color: "box36"}
       ]
-      console.log("init navbar")
 
       // vm.getBoxes();
       // console.log(document.getElementById("2"))
+
+      //pagination index
+      vm.iPgn = 0;
+
+      //used to calculate scroll pagination.
+      vm.headerHeight = 64;
+      vm.topSpacerHeight = 0;
+      vm.boxDivHeight = 316;
+
+      vm.pgnThresh = 100; //vm.headerHeight + vm.topSpacerHeight + (2*vm.boxDivHeight);
+      vm.numItems = 3;
+      vm.pageYOffsetAcm = 0;
+      vm.prevPageYOffset = 0;
+      //slice from index to nth item (+3 for initialization)
+      vm.iFrom = vm.iPgn/vm.pgnThresh * vm.numItems;
+      vm.iTo = vm.iFrom * vm.numItems + vm.numItems;
+      vm.curBoxes = vm.allBoxes.slice(vm.iFrom, vm.iTo+6);
+
+
+
+      console.log("from to: ", vm.iFrom, vm.iTo)
+
+      //put a listener on scroll event to update pagination
+      window.addEventListener('scroll', function(e) {
+        console.log(vm.pageYOffsetAcm)
+        // console.log("scrolling: window.pageYOffset: ", window.pageYOffset)
+        // console.log("scrolling: vm.pageYOffsetAcm: ", vm.pageYOffsetAcm)
+        vm.pageYOffsetAcm+= window.pageYOffset-vm.prevPageYOffset;
+        vm.prevPageYOffset = window.pageYOffset;
+
+        //on large screens:
+        //when scrolling past 2 rows, delete two rows, load up two more (6 items).
+        if(Math.abs(vm.pageYOffsetAcm - vm.iPgn) >= vm.pgnThresh) {
+          //300, 600, 900
+          //if scrolling down
+          console.log("down: vm.iPgn: ", vm.iPgn)
+          if(vm.pageYOffsetAcm > vm.iPgn) {
+            vm.iPgn+= vm.pgnThresh
+            vm.prevPageYOffset = 0; //reset bc scroll height of window stays same when new stuff added (Since onld stuff deleted)
+            vm.updatePage(1);
+          } else {
+            //if scrolling up
+            vm.iPgn-= vm.pgnThresh
+            vm.updatePage(-1);
+          }
+        }
+
+      })
     };
 
-    vm.purgeBoxes = function() {
+    //called when first two rows of loaded boxes goes off screen
+    vm.updatePage = function(direction) {
+      if(direction === 1) {
+        // console.log("down iPgn: ", vm.iPgn)
+        // console.log("down");
+        let boxes = document.getElementById("boxes");
+        let boxesChildren = document.getElementById("boxes").children;
 
+        for(let i=0; i < vm.numItems; i++) {
+          boxes.removeChild(boxesChildren[0]);
+        }
+        //jquery this for now.. figure out how to replace with angular-correct way
+        /*
+        <div class="post-div col s2 m4 l4" ng-repeat="box in $ctrl.curBoxes">
+          <p>{{ box.color }}</p>
+          <canvas id={{box.id}} class="canvas-style" canvas-init></canvas>
+        </div>
+        */
+        for(i=0; i < vm.numItems; i++) {
+          // console.log("vm.iPgn: ", vm.iPgn)
+          // console.log("vm.iPgn/vm.pgnThresh: ", vm.iPgn/vm.pgnThresh)
+          let iAllBoxes = vm.iPgn/vm.pgnThresh*vm.numItems+i;
+          let div = $('<div>').addClass("post-div col s2 m4 l4")
+          let p = $('<p>').text(vm.allBoxes[iAllBoxes+6].color)
+          let canvas = $('<canvas>')
+          div.append(p);
+          div.append(canvas);
+          canvas.addClass('canvas-style');
+          $("#boxes").append(div);
+          vm.genBox(vm.allBoxes[iAllBoxes+6], canvas[0]);
+        }
+      } else {
+        console.log("up")
+      }
+      //delete first six loaded boxes
+      //load next six boxes
     }
+
+/*
+    const flexColDiv = $('<div>').addClass('helper-flex-col');
+      const rightIcon = $('<i>').addClass('circle-right material-icons').text('account_circle');
+      flexColDiv.append(rightIcon);
+
+      if (name) {
+        const messageIcon = $('<i>').addClass('material-icons helper-icon').text('message');
+        const helperNameP = $('<p>').text(name).attr('id','helperName')
+        flexDiv.append(messageIcon);
+        flexColDiv.append(helperNameP)
+      } else {
+        rightIcon.addClass('grey-text text-lighten-2')
+      }
+
+      flexDiv.append(flexColDiv);
+      rightItemDiv.append(flexDiv);
+      avatarDiv.append(rightItemDiv);
+*/
 
     vm.test = function() {
       console.log("snarfff")
     }
     //generate box from parameters from box description from database
     vm.genBox = function (box, canvas) {
-      console.log("cur canv: ", canvas)
+      // console.log("cur box", box)
+      // console.log("cur canv: ", canvas)
       // let currentCanvas = (box.id).toString();
       // console.log(currentCanvas)
       // console.log(document.getElementById("2"))
@@ -151,6 +265,10 @@
     }
 
     vm.getBoxes = function() {
+      //load up all the boxes to front end but do not render them.
+      //rendering will be front-end paginated
+      // (todo: paginate this load)
+
       //request this endpoint from server.js
       $http.get('/api/boxes').then(function (response) {
         vm.boxes = response.data;
