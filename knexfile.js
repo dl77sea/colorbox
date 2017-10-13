73e5ddd0c4c2bed89400b3b29cbda6ec678e7b5a
Daniel Larsson <dl77sea@Daniels-MacBook-Air.local>
2017-10-13 16:54:00 -0700

    ..

--- knexfile.js
+++ knexfile.js
@@ -8,9 +8,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      database: process.env.DATABASE_URL,
-    }
+    connection: database: process.env.DATABASE_URL
   }
 };
 
